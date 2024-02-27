c730e76d448ef8bc38c0ae162f5bde11253ac1d8
sbk850 <sbk850@outlook.com>
2024-02-27 20:51:52 +0000

    Auto-commit

--- fetchAndDisplayContent.js
+++ fetchAndDisplayContent.js
@@ -1,3 +1,86 @@
+async function fetchAndDisplayContent(postUrl, bar, submitBtn, form, contentContainer) {
+    showProgressBar(bar);
+    try {
+        const apiEndpoint = 'https://cyberguardians.onrender.com/scrape';
+        const response = await fetch(apiEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ url: postUrl }),
+        });
+        if (!response.ok) {
+            throw new Error(`Network response was not ok, status: ${response.status}`);
+        }
+        const reader = response.body.getReader();
+        const contentLength = +response.headers.get('Content-Length');
+        let receivedLength = 0;
+        let chunks = [];
+        while (true) {
+            const { done, value } = await reader.read();
+            if (done) {
+                break;
+            }
+            chunks.push(value);
+            receivedLength += value.length;
+            let loadingPercentage = (receivedLength / contentLength) * 100;
+            bar.style.width = `${loadingPercentage}%`;
+        }
+        let chunksAll = new Uint8Array(receivedLength);
+        let position = 0;
+        for (let chunk of chunks) {
+            chunksAll.set(chunk, position);
+            position += chunk.length;
+        }
+        let result = new TextDecoder("utf-8").decode(chunksAll);
+        let jsonData = JSON.parse(result);
+        const postData = jsonData[0];
+        document.getElementById('profileImageUrl').src = postData.ProfilePictureURL || 'placeholder-image-url.png';
+        document.getElementById('posterName').textContent = `${postData.FirstName} ${postData.LastName}` || 'Name not available';
+        document.getElementById('posterDetails').textContent = `Age: ${postData.Age} | Education: ${postData.Education}` || 'Details not available';
+        document.getElementById('postContent').textContent = postData.Content || 'Content not available';
+        contentContainer.style.display = 'block';
+        form.style.display = 'none';
+    } catch (error) {
+        console.error('Fetch Error:', error);
+        contentContainer.style.display = 'none';
+        form.style.display = 'block';
+    } finally {
+        hideProgressBar(bar);
+        submitBtn.style.display = 'none';
+    }
+}
+
+async function analyseContentForToxicity(content) {
+    try {
+        const analysisEndpoint = '/analyse-content';
+        const response = await fetch(analysisEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ content: content }),
+        });
+        if (!response.ok) {
+            throw new Error(`Network response was not ok, status: ${response.status}`);
+        }
+        const analysisResult = await response.json();
+        document.getElementById('toxicityScore').textContent = `Toxicity Score: ${analysisResult.score}`;
+    } catch (error) {
+        console.error('Error analyzing content:', error);
+    }
+}
+
+function isValidUrl(url) {
+    var pattern = new RegExp('^(https?:\\/\\/)?' +
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+        '((\\d{1,3}\\.){3}\\d{1,3}))' +
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+        '(\\?[;&a-z\\d%_.~+=-]*)?' +
+        '(\\#[-a-z\\d_]*)?$', 'i');
+    return pattern.test(url);
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector('#reportForm');
     const contentContainer = document.querySelector('#content');
@@ -16,7 +99,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const postUrl = postUrlInput.value;
         if (isValidUrl(postUrl)) {
             try {
-                await fetchAndDisplayContent(postUrl, contentContainer);
+                await fetchAndDisplayContent(postUrl, bar, submitBtn, form, contentContainer);
             } catch (error) {
                 console.error('Error fetching and displaying content:', error);
             }
@@ -81,91 +164,5 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             }
         }, 100);
-    }
-
-    async function fetchAndDisplayContent(postUrl, bar, submitBtn, form, contentContainer) {
-        showProgressBar(bar); // Change to use 'bar' instead of 'progressbar'
-        try {
-            const apiEndpoint = 'https://cyberguardians.onrender.com/scrape';
-            const response = await fetch(apiEndpoint, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ url: postUrl }),
-            });
-            if (!response.ok) {
-                throw new Error(`Network response was not ok, status: ${response.status}`);
-            }
-            const reader = response.body.getReader();
-            const contentLength = +response.headers.get('Content-Length');
-            let receivedLength = 0;
-            let chunks = [];
-            while (true) {
-                const { done, value } = await reader.read();
-                if (done) {
-                    break;
-                }
-                chunks.push(value);
-                receivedLength += value.length;
-                let loadingPercentage = (receivedLength / contentLength) * 100;
-                bar.style.width = `${loadingPercentage}%`;
-            }
-            let chunksAll = new Uint8Array(receivedLength);
-            let position = 0;
-            for (let chunk of chunks) {
-                chunksAll.set(chunk, position);
-                position += chunk.length;
-            }
-            let result = new TextDecoder("utf-8").decode(chunksAll);
-            let jsonData = JSON.parse(result);
-            const postData = jsonData[0];
-            document.getElementById('profileImageUrl').src = postData.ProfilePictureURL || 'placeholder-image-url.png';
-            document.getElementById('posterName').textContent = `${postData.FirstName} ${postData.LastName}` || 'Name not available';
-            document.getElementById('posterDetails').textContent = `Age: ${postData.Age} | Education: ${postData.Education}` || 'Details not available';
-            document.getElementById('postContent').textContent = postData.Content || 'Content not available';
-            contentContainer.style.display = 'block';
-            form.style.display = 'none';
-        } catch (error) {
-            console.error('Fetch Error:', error);
-            contentContainer.style.display = 'none';
-            form.style.display = 'block';
-        } finally {
-            hideProgressBar(bar); // Change to use 'bar' instead of 'progressbar'
-            submitBtn.style.display = 'none';
-        }
-    }
-    
-
-    analyseContentForToxicity(postData.Content || '');
-
-    async function analyseContentForToxicity(content) {
-        try {
-            const analysisEndpoint = '/analyse-content';
-            const response = await fetch(analysisEndpoint, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ content: content }),
-            });
-            if (!response.ok) {
-                throw new Error(`Network response was not ok, status: ${response.status}`);
-            }
-            const analysisResult = await response.json();
-            document.getElementById('toxicityScore').textContent = `Toxicity Score: ${analysisResult.score}`;
-        } catch (error) {
-            console.error('Error analyzing content:', error);
-        }
-    }
-
-    function isValidUrl(url) {
-        var pattern = new RegExp('^(https?:\\/\\/)?' +
-            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
-            '((\\d{1,3}\\.){3}\\d{1,3}))' +
-            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
-            '(\\?[;&a-z\\d%_.~+=-]*)?' +
-            '(\\#[-a-z\\d_]*)?$', 'i');
-        return pattern.test(url);
     }
 });
