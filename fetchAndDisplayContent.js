28414fc83c5ced7b078a1b01b2940d54fea5db86
sbk850 <sbk850@outlook.com>
2024-02-27 20:37:57 +0000

    Auto-commit

--- fetchAndDisplayContent.js
+++ fetchAndDisplayContent.js
@@ -137,13 +137,13 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    function isValidUrl(url) {
-        var pattern = new RegExp('^(https?:\\/\\/)?' +
-            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
-            '((\\d{1,3}\\.){3}\\d{1,3}))' +
-            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
-            '(\\?[;&a-z\\d%_.~+=-]*)?' +
-            '(\\#[-a-z\\d_]*)?$', 'i');
-        return pattern.test(url);
-    }
+    // function isValidUrl(url) {
+    //     var pattern = new RegExp('^(https?:\\/\\/)?' +
+    //         '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+    //         '((\\d{1,3}\\.){3}\\d{1,3}))' +
+    //         '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+    //         '(\\?[;&a-z\\d%_.~+=-]*)?' +
+    //         '(\\#[-a-z\\d_]*)?$', 'i');
+    //     return pattern.test(url);
+    // }
 });
