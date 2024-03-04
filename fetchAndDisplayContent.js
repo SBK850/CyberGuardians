b6cbdd57dac8b6c113e6c72188fb0f9a32fba136
sbk850 <sbk850@outlook.com>
2024-03-04 14:31:25 +0000

    Auto-commit

--- fetchAndDisplayContent.js
+++ fetchAndDisplayContent.js
@@ -201,63 +201,23 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-var basicTimeline = anime.timeline({
-    autoplay: false
-});
+$(function () {
+    var btn = $(".btn");
 
-var pathEls = $(".check");
-for (var i = 0; i < pathEls.length; i++) {
-    var pathEl = pathEls[i];
-    var offset = anime.setDashoffset(pathEl);
-    pathEl.setAttribute("stroke-dashoffset", offset);
-}
+    btn.on("click", function () {
 
-basicTimeline
-    .add({
-        targets: ".text",
-        duration: 1,
-        opacity: "0"
-    })
-    .add({
-        targets: ".button",
-        duration: 1300,
-        height: 10,
-        width: 300,
-        backgroundColor: "#2B2D2F",
-        border: "0",
-        borderRadius: 100
-    })
-    .add({
-        targets: ".progress-bar",
-        duration: 2000,
-        width: 300,
-        easing: "linear"
-    })
-    .add({
-        targets: ".button",
-        width: 0,
-        duration: 1
-    })
-    .add({
-        targets: ".progress-bar",
-        width: 80,
-        height: 80,
-        delay: 500,
-        duration: 750,
-        borderRadius: 80,
-        backgroundColor: "#71DFBE"
-    })
-    .add({
-        targets: pathEl,
-        strokeDashoffset: [offset, 0],
-        duration: 200,
-        easing: "easeInOutSine"
+        $(this).addClass('btn-progress');
+        setTimeout(function () {
+            btn.addClass('btn-fill')
+        }, 500);
+
+        setTimeout(function () {
+            btn.removeClass('btn-fill')
+        }, 4100);
+
+        setTimeout(function () {
+            btn.addClass('btn-complete')
+        }, 4100);
+
     });
-
-$(".button").click(function () {
-    basicTimeline.play();
-});
-
-$(".text").click(function () {
-    basicTimeline.play();
-});
+})
