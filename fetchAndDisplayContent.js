8423f1bfe1c4285a86a33758dc15cecd1d382ed8
sbk850 <sbk850@outlook.com>
2024-03-05 23:53:21 +0000

    Auto-commit

--- fetchAndDisplayContent.js
+++ fetchAndDisplayContent.js
@@ -251,6 +251,21 @@ document.addEventListener('DOMContentLoaded', () => {
         const matches = url.match(/^https?:\/\/([^\/]+)/i);
         return matches && matches[1] ? matches[1].replace('www.', '').toLowerCase() : '';
     }
+
+    async function confirmToxicContent(carouselItemId) {
+        // Handle confirmation action here
+        try {
+            await removeToxicPost(carouselItemId);
+            var message = document.createElement('p');
+            message.textContent = "You have confirmed the removal of this content. It will be removed immediately from YouthVibe. Thank you for helping us maintain a safe environment.";
+            document.getElementById("message-section").appendChild(message);
+            document.getElementById("warning-section").style.display = "none";
+            document.getElementById("message-section").style.display = "block";
+        } catch (error) {
+            console.error('Error confirming toxic content:', error);
+        }
+    }
+
 });
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -272,18 +287,4 @@ function rejectToxicContent() {
     document.getElementById("message-section").appendChild(message);
     document.getElementById("warning-section").style.display = "none";
     document.getElementById("message-section").style.display = "block";
-}
-
-async function confirmToxicContent(carouselItemId) {
-    // Handle confirmation action here
-    try {
-        await removeToxicPost(carouselItemId);
-        var message = document.createElement('p');
-        message.textContent = "You have confirmed the removal of this content. It will be removed immediately from YouthVibe. Thank you for helping us maintain a safe environment.";
-        document.getElementById("message-section").appendChild(message);
-        document.getElementById("warning-section").style.display = "none";
-        document.getElementById("message-section").style.display = "block";
-    } catch (error) {
-        console.error('Error confirming toxic content:', error);
-    }
 }
