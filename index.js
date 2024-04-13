cc435ef7224e4ed33a6fa64467739af5ab662bf1
sbk850 <sbk850@outlook.com>
2024-04-13 03:40:52 +0100

    Auto-commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+    const openButton = document.querySelector('.open-navigation');
+    const closeButton = document.querySelector('.close-navigation');
+    const navigation = document.querySelector('.site-navigation');
+
+    openButton.addEventListener('change', function () {
+        if (this.checked) {
+            navigation.style.visibility = 'visible';
+        }
+    });
+
+    closeButton.addEventListener('change', function () {
+        if (this.checked) {
+            navigation.style.visibility = 'hidden';
+        }
+    });
+});
