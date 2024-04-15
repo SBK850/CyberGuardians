518c4e0dfbab577612d84ec82f7f34c3c3b8c815
sbk850 <sbk850@outlook.com>
2024-04-16 00:06:52 +0100

    Auto-commit

--- fetchAndDisplayContent.js
+++ fetchAndDisplayContent.js
@@ -141,7 +141,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     async function fetchAndDisplayContent(postUrl, contentContainer) {
         try {
-            const response = await fetch('https://lambda-cyberguardians.vercel.app/scrape', {
+            const response = await fetch('https://lambda-cyberguardians-sbk850s-projects.vercel.app/scrape', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
